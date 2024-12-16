9842d10b4bbc7e9c717aa5f28746c387a1ede44a
Pukkok <krystalgggg@gmail.com>
2024-12-16 17:40:34 +0900

    기본 단축키 추가

--- src/constants/pianoItems.js
+++ src/constants/pianoItems.js
@@ -1,5 +1,17 @@
-const tunes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A',"A#", "B", "C"]
-const pithches = [1, 2, 3, 4, 5, 6, 7]
+const tunes = ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4',"A#4", "B4", "C5"]
+const pithches = [3, 4, 5]
 const koreanNames = ["도", "도#", "레", "레#", "미", "파", "파#", "솔", "솔#", "라", "라#", "시", "도"]
 
-export { tunes, pithches, koreanNames }
+const defaultShorcut = {
+  'A' : 'C4', 'W' : 'C#4', //도
+  'S' : 'D4', 'E' : 'D#4', //레
+  'D' : 'E4', //미
+  'F' : 'F4', 'T' : 'F#4', //파
+  'G' : 'G4', 'Y' : 'G#4', //솔 
+  'H' : 'A4', 'U' : 'A#4', //라
+  'J' : 'B4', //시
+  'K' : 'C5', 'O' : 'C#5', //도
+  'L' : 'D5', 'P' : 'D#5' //레
+}
+
+export { tunes, pithches, koreanNames, defaultShorcut }
