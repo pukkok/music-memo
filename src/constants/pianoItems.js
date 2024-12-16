7280fab6db9e75d14eeff9fa46db51070f6c3ffa
Pukkok <krystalgggg@gmail.com>
2024-12-16 15:48:07 +0900

    상수로 쓸 아이템 생성

---
+++ src/constants/pianoItems.js
@@ -0,0 +1,5 @@
+const tunes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A',"A#", "B", "C"]
+const pithches = [1, 2, 3, 4, 5, 6, 7]
+const koreanNames = ["도", "도#", "레", "레#", "미", "파", "파#", "솔", "솔#", "라", "라#", "시", "도"]
+
+export { tunes, pithches, koreanNames }
