e4847c91032f9bd8e8b848543995750109e3159b
Pukkok <krystalgggg@gmail.com>
2024-12-16 14:59:36 +0900

    상수 설정 수정

--- src/pianoConstant.js
+++ src/pianoConstant.js
@@ -1,12 +1,6 @@
-const notes = [
-  'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1',
-  'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2',
-  'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
-  'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
-  'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',
-  'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6',
-  'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7', 'C8'
-]
+const tunes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A',"A#", "B", "C"]
+const pithches = [1, 2, 3, 4, 5, 6, 7]
+const koreanNames = ["도", "도#", "레", "레#", "미", "파", "파#", "솔", "솔#", "라", "라#", "시", "도"]
 
 const defaultShorcut = {
   'C4' : 'A', 'C#4' : 'W', //도
@@ -19,12 +13,6 @@ const defaultShorcut = {
   'C5' : 'K', 'C#5' : 'O', //도
   'D5' : 'L', 'D#5' : 'P' //레
 }
-
-const scoreTitles = [
-  '네가있어행복해', 
-  '나무의노래',
-  '달팽이의하루',
-]
 
 const allInstruments = [
   "accordion", "acoustic_bass", "acoustic_grand_piano", "acoustic_guitar_nylon", "acoustic_guitar_steel",
@@ -50,3 +38,17 @@ const allInstruments = [
   "trombone", "trumpet", "tuba", "tubular_bells", "vibraphone", "viola", 
   "violin", "voice_oohs", "whistle", "woodblock", "xylophone"
 ]
+
+export { tunes }
+
+// 음계 정보 배열
+// const notes = [
+//   { color: "#F28B82", pitch: "C4", koreanName: "도", englishName: "Do" },
+//   { color: "#FBAF85", pitch: "D4", koreanName: "레", englishName: "Re" },
+//   { color: "#F2D769", pitch: "E4", koreanName: "미", englishName: "Mi" },
+//   { color: "#A3D181", pitch: "F4", koreanName: "파", englishName: "Fa" },
+//   { color: "#78C0E0", pitch: "G4", koreanName: "솔", englishName: "Sol" },
+//   { color: "#93B0C3", pitch: "A4", koreanName: "라", englishName: "La" },
+//   { color: "#C8A2C8", pitch: "B4", koreanName: "시", englishName: "Si" },
+//   { color: "#F28B82", pitch: "C5", koreanName: "도", englishName: "Do" },
+// ]
