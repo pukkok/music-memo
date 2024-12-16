6f940e5e530e9a14962d388630963711269b93d4
Pukkok <krystalgggg@gmail.com>
2024-12-16 15:00:28 +0900

    canvas, board연결

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Header from './header/header'
+import Canvas from './canvas/canvas'
+import Board from './board/board'
 import './style.css'
 
 document.querySelector('#app').append(
-  Header
+  Header, Canvas, Board
 )
