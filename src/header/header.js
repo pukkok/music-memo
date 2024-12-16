fde813403b83eadc2722930f9303853942a32613
Pukkok <krystalgggg@gmail.com>
2024-12-16 15:15:43 +0900

    처음부터 시작하기

--- src/header/header.js
+++
@@ -1,6 +0,0 @@
-const Header = document.createElement('header')
-Header.innerHTML =`
-  <h1>음악 메모장</h1>
-`
-
-export default Header
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import Header from './header/header'
-import Canvas from './canvas/canvas'
-import Board from './board/board'
-import './style.css'
-
-document.querySelector('#app').append(
-  Header, Canvas, Board
-)
