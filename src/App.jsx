a8622ebe2d5627c0c0ad0eb68bb8921267448c8a
Pukkok <krystalgggg@gmail.com>
2024-12-17 17:42:14 +0900

    노트의 마지막 값을 지울 수 있도록 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import Piano from './component/piano'
+import Controls from './component/controls'
 import './App.css'
 import Container from './component/container'
 import NoteCanvas from './canvas/noteCanvas'
 import { useState } from 'react'
-import Controls from './component/controls'
 
 function App() {
   const [pressed, setPressed] = useState([]) // 현재 누른 키들
-  const [memo, setMemo] = useState([]) // 직사각형 메모 데이터
+  const [memo, setMemo] = useState([]) // 메모 데이터
+
+  // 메모 초기화 함수
+  const handleReset = () => {
+    setMemo([])
+  }
+  const handleDelete = () => {
+    setMemo(prev => prev.filter((_, idx) => idx !== prev.length - 1))
+  }
 
   return (
     <Container>
       <NoteCanvas memo={memo} />
       <Piano pressed={pressed} setPressed={setPressed} setMemo={setMemo} />
-      <Controls memo={memo}/>
+      <Controls memo={memo} onReset={handleReset} onDelete={handleDelete}/>
     </Container>
   )
 }
--- src/component/controls.jsx
+++ src/component/controls.jsx
@@ -2,8 +2,7 @@ import soundManager from '../utils/soundManager'
 import './styles/controls.css'
 import { useEffect } from 'react'
 
-const Controls = ({memo}) => {
-
+const Controls = ({ memo, onReset, onDelete }) => {
   useEffect(() => {
     soundManager.loadInstrument()
   }, [])
@@ -12,6 +11,8 @@ const Controls = ({memo}) => {
     <div className="controls">
       <button onClick={() => soundManager.playSequence(memo)}>시작</button>
       <button onClick={() => soundManager.stopSequence()}>정지</button>
+      <button onClick={onReset}>초기화</button>
+      <button onClick={onDelete}>마지막 값 지우기</button>
     </div>
   )
 }
