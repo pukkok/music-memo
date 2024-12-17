70a3273a9bd35567dce5c6b3b19a5903fa2c3b36
Pukkok <krystalgggg@gmail.com>
2024-12-17 16:15:23 +0900

    메모 소리가 promise를 통해 순차적으로 소리나도록 구현

--- src/utils/soundManager.js
+++ src/utils/soundManager.js
@@ -5,6 +5,7 @@ class SoundManager {
     this.audioContext = new (window.AudioContext || window.webkitAudioContext)()
     this.player = null
     this.activeNotes = {}
+    this.playingSequence = null
   }
 
   async loadInstrument(instrument = 'acoustic_grand_piano') {
@@ -21,6 +22,35 @@ class SoundManager {
     if (this.activeNotes[note]) {
       this.activeNotes[note].stop()
       delete this.activeNotes[note]
+    }
+  }
+
+  playSequence(notes) {
+    this.stopSequence() // 기존 재생 중단
+  
+    if (!Array.isArray(notes) || notes.length === 0) {
+      console.warn('playSequence expects a non-empty array')
+      return
+    }
+  
+    this.playingSequence = notes.reduce((promise, { note, duration }) => {
+      return promise.then(() => {
+        return new Promise((resolve) => {
+          this.playNote(note)
+          setTimeout(() => { 
+            this.stopNote(note)
+            resolve()
+          }, duration)
+        })
+      })
+    }, Promise.resolve())
+  }
+  
+
+  stopSequence() {
+    if (this.playingSequence) {
+      this.playingSequence = null
+      Object.keys(this.activeNotes).forEach((note) => this.stopNote(note))
     }
   }
 }
