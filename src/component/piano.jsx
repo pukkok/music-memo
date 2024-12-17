3352791edd3d42841e4c2342cddf32080e1c2caf
Pukkok <krystalgggg@gmail.com>
2024-12-17 10:33:12 +0900

    키보드 이벤트 메모 작업 수정

--- src/component/piano.jsx
+++ src/component/piano.jsx
@@ -1,24 +1,28 @@
 import { tunes, defaultShorcut } from '../constants/pianoItems'
 import './styles/piano.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
-const Piano = ({pressed, setPressed}) => {
-
-  const noteClick = () => {
-    console.log('노트 클릭')
-  }
+const Piano = ({ pressed, setPressed, setMemo }) => {
+  const [startTime, setStartTime] = useState({}) // 시작 시간 저장
 
   const noteKeyDown = (e) => {
     const pressedNote = defaultShorcut[e.key.toUpperCase()]
-    // console.log('노트 키다운')
-    setPressed(prev => {
-      return prev.includes(pressedNote) ? prev : [...prev, pressedNote]
-    })
+    if (pressedNote && !pressed.includes(pressedNote)) {
+      setPressed((prev) => [...prev, pressedNote]) // 키 정보 저장
+      setStartTime((prev) => ({ ...prev, [pressedNote]: Date.now() })) // 시작 시간 기록
+    }
   }
+
   const noteKeyUp = (e) => {
     const pressedNote = defaultShorcut[e.key.toUpperCase()]
-    // console.log('노트 키업')
-    setPressed(prev => prev = prev.filter(key => key !== pressedNote))
+    if (pressedNote && pressed.includes(pressedNote)) {
+      const duration = Date.now() - startTime[pressedNote] // 지속 시간 계산
+      setMemo((prev) => [
+        ...prev,
+        { note: pressedNote, duration: Math.round(duration / 250) * 250 }, // 반올림된 duration
+      ])
+      setPressed((prev) => prev.filter((key) => key !== pressedNote)) // pressed에서 제거
+    }
   }
 
   useEffect(() => {
@@ -28,19 +32,18 @@ const Piano = ({pressed, setPressed}) => {
       window.removeEventListener('keydown', noteKeyDown)
       window.removeEventListener('keyup', noteKeyUp)
     }
-  }, [])
+  }, [pressed, startTime])
 
-  return <div className="notes">
-    {tunes.map((note, idx) => {
-      return <button 
-        key={idx} 
-        className={`
-          ${note.includes("#") ? "sharp" : ""}
-          ${pressed.includes(note) && "active"} `}
-        onClick={noteClick}
+  return (
+    <div className="notes">
+      {tunes.map((note, idx) => (
+        <button
+          key={idx}
+          className={`${pressed.includes(note) && 'active'} ${note.includes('#') && 'sharp'}`}
         ></button>
-    })}
-  </div>
+      ))}
+    </div>
+  )
 }
 
 export default Piano
