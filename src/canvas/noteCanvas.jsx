787f83cddddfc91b5add3e56e39b29cb73b781a1
Pukkok <krystalgggg@gmail.com>
2024-12-17 10:33:24 +0900

    메모를 받아서 그릴수 있도록 변경

--- src/canvas/noteCanvas.jsx
+++ src/canvas/noteCanvas.jsx
@@ -1,19 +1,38 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from 'react'
+import { koreanNames, tunes } from '../constants/pianoItems'
 
-const NoteCanvas = ({pressed}) => {
+const noteColors = [
+  '#ff9999', '#ffcc99', '#ffff99', '#ccff99', '#99ffcc', '#99ccff', '#cc99ff', '#f9b7ff',
+]
+
+const NoteCanvas = ({ memo }) => {
   const canvasRef = useRef()
-  
+
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
-
     canvas.width = 1240
     canvas.height = 800
 
-    ctx.fillStyle = "pink"
+    ctx.clearRect(0, 0, canvas.width, canvas.height) // 캔버스 초기화
+    ctx.fillStyle = 'white'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
-    ctx.strokeRect(0, 0, canvas.width, canvas.height)
-  }, [pressed])
+
+    let xOffset = 0 // 누적된 X 좌표
+
+    memo.forEach(({ note, duration }) => {
+      const color = noteColors[tunes.indexOf(note) % noteColors.length]
+      const label = `${koreanNames[tunes.indexOf(note)]}4`
+      const width = (duration / 1000) * 100 // 1초 = 100px
+
+      ctx.fillStyle = color
+      ctx.fillRect(xOffset, 50, width, 50)
+      ctx.fillStyle = 'black'
+      ctx.fillText(label, xOffset + 5, 65)
+
+      xOffset += width // 다음 노트의 시작 X 위치
+    })
+  }, [memo])
 
   return <canvas ref={canvasRef}></canvas>
 }
