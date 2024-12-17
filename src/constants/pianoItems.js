a777a235daadcb4185921aff5a2480ca0bc4f488
Pukkok <krystalgggg@gmail.com>
2024-12-17 15:27:02 +0900

    단축키와 기본 보여주는 음 늘리기

--- src/constants/pianoItems.js
+++ src/constants/pianoItems.js
@@ -1,4 +1,4 @@
-const tunes = ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5']
+const tunes = ['C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5', 'E5']
 const koreanNames = ['도', '도#', '레', '레#', '미', '파', '파#', '솔', '솔#', '라', '라#', '시', '도']
 
 const defaultShorcut = {
@@ -18,6 +18,7 @@ const defaultShorcut = {
   O: 'C#5',
   L: 'D5',
   P: 'D#5',
+  ";": 'E5',
 }
 
 export { tunes, koreanNames, defaultShorcut }
