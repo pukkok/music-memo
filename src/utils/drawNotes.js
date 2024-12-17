cc2e6e904c5d9e28e417d4e07c86f5ae57cc4668
Pukkok <krystalgggg@gmail.com>
2024-12-17 15:31:02 +0900

    한글 메모가 정확히 나오도록 옥타브 기준으로 변경

--- src/utils/drawNotes.js
+++ src/utils/drawNotes.js
@@ -1,11 +1,13 @@
 import { noteColors, LINE_PADDING, RECT_HEIGHT } from "../constants/canvasLayouts"
 import { koreanNames, tunes } from "../constants/pianoItems"
 
-// 메모 그리기
+// * 메모 그리기
 const drawNotes = (ctx, currentPageMemo) => {
+  const ONE_OCTAVE = 12 // ? 도 ~ 시 까지 총 들어가는 음의 수(반음 포함)
+
   currentPageMemo.forEach(({ note, xOffset, yOffset, width }) => {
     const color = noteColors[tunes.indexOf(note) % noteColors.length]
-    const label = `${koreanNames[tunes.indexOf(note)]}${note.split('').pop()}`
+    const label = `${koreanNames[tunes.indexOf(note) % ONE_OCTAVE]}${note.split('').pop()}`
 
     ctx.fillStyle = color
     ctx.fillRect(xOffset, yOffset + LINE_PADDING, width, RECT_HEIGHT)
