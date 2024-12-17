22704118a4563fcebe98b3f2261744c35a01994d
Pukkok <krystalgggg@gmail.com>
2024-12-17 13:51:00 +0900

    레이아웃 설정값 개별로 관리하기

---
+++ src/constants/canvasLayouts.js
@@ -0,0 +1,10 @@
+const noteColors = [
+  '#ff9999', '#ffcc99', '#ffff99', '#ccff99', '#99ffcc', '#99ccff', '#cc99ff', '#f9b7ff',
+]
+
+const NOTEBOOK_HEIGHT = 100 // 공책 한 줄의 높이
+const RECT_HEIGHT = 60 // 직사각형 높이
+const LINE_PADDING = 20 // 세로 중앙정렬 기준
+const NOTE_SPACING = 5 // 노트와 노트 사이 간격
+
+export { noteColors, NOTEBOOK_HEIGHT, RECT_HEIGHT, LINE_PADDING, NOTE_SPACING }
