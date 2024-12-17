da4993ee40366f2b699ce9fb5ec401935d7153e9
Pukkok <krystalgggg@gmail.com>
2024-12-17 22:01:47 +0900

    아이들 아이콘으로 전체 변경

--- src/component/controls.jsx
+++ src/component/controls.jsx
@@ -2,17 +2,17 @@ import soundManager from '../utils/soundManager'
 import './styles/controls.css'
 import { useEffect } from 'react'
 
-const Controls = ({ memo, onReset, onDelete }) => {
+const Controls = ({ memo, onReset, onDelete, pinsetIndex }) => {
   useEffect(() => {
     soundManager.loadInstrument()
   }, [])
 
   return (
     <div className="controls">
-      <button onClick={() => soundManager.playSequence(memo)}>시작</button>
-      <button onClick={() => soundManager.stopSequence()}>정지</button>
-      <button onClick={onReset}>초기화</button>
-      <button onClick={onDelete}>마지막 값 지우기</button>
+      <button onClick={() => soundManager.playSequence(memo, pinsetIndex)}>🟩</button>
+      <button onClick={() => soundManager.stopSequence()}>🟥</button>
+      <button onClick={onReset}>💣</button>
+      <button onClick={onDelete}>🗑️</button>
     </div>
   )
 }
