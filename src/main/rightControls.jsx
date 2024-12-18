be855178a720ca0505f5e1ed580dcbb49bc5b0a3
Pukkok <krystalgggg@gmail.com>
2024-12-18 10:06:52 +0900

    횡단보도 이미지 추가

--- src/main/rightControls.jsx
+++ src/main/rightControls.jsx
@@ -4,6 +4,7 @@ const RightControls = ({onReset, onDelete}) => {
 
   return (
     <div className='right-controls'>
+      <button><img src='./public/crosswalk.png' alt=''/></button>
       <button onClick={onReset}>💣</button>
       <button onClick={onDelete}>🗑️</button>
     </div>
